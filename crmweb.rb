4ba44c6677796c8abd14ef58396a8be21d5a9d6a
Cody <codyjjwbrown@gmail.com>
2015-04-10 16:25:16 -0400

    the site is now all done up in CSS, with a footer, header, sidebar

--- crmweb.rb
+++ crmweb.rb
@@ -1,35 +1,40 @@
 require_relative 'contact'
 require_relative 'rolodex'
-require 'sinatra'
+require 'haml'
+require 'rubygems'
+require 'sinatra/base'
 
 $rolodex= Rolodex.new
 
 
+class CrmApp < Sinatra::Base
+
+	get '/' do
+		# @crm_app_name = "My CRM"
+		# erb :index
+		haml :index
+	end
+
+	get "/contacts" do
+
+		haml :contacts
+	end
+
+	get '/newcontacts' do
+		haml :newcontacts
+	end
+
+	get '/about' do
+		haml :about
+	end
 
 
+	post '/contacts' do
+		new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+		$rolodex.add_contact(new_contact)
+		redirect to('/')
+	end
 
-
-#-------Routes----------
-
-
-get '/' do
-	@crm_app_name = "My CRM"
-	erb :index
 end
 
-get "/contacts" do
-
-	erb :contacts
-end
-
-get '/contacts/new' do
-	erb :new_contact
-end
-
-post '/contacts' do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-	$rolodex.add_contact(new_contact)
-	redirect to('/contacts')
-end
-
-
+CrmApp.run!
---
+++ run_crmweb.rb
@@ -0,0 +1,3 @@
+require 'crmweb'
+
+CrmApp.run!
